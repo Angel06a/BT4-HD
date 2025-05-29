08425e05ac7c562252fb8d19e52315dbb2c159bc
Angel06 <ug25228@gmail.com>
2025-05-29 16:25:57 -0500

    Add files via upload

---
+++ img.js
@@ -0,0 +1,8 @@
+const imgData = `
+-img1.webp
+-img2.webp
+-img3.webp
+-img4.webp
+-img5.webp
+-img6.webp
+`;
---
+++ list.js
@@ -0,0 +1,5 @@
+// list.js
+const listData = `
+-Texture Pack HD "https://www.mediafire.com/file/2jjsfjtxqmhbzov/dbz_bt4_hd_v1.0.zip/file"
+-Explicación de instalación "https://www.mediafire.com/file/iqv5v82qrkvcufy/Explicaci%25C3%25B3n_de_instalaci%25C3%25B3n_de_texturas.txt/file"
+`;
