a7e232a74e9254f4b9879621ed5bc48d26aedac5
Angel06 <ug25228@gmail.com>
2025-06-06 18:29:28 -0500

    Update list.js

--- list.js
+++ list.js
@@ -1,5 +1,5 @@
 // list.js
 const listData = `
--Texture Pack HD "https://www.mediafire.com/file/2jjsfjtxqmhbzov/dbz_bt4_hd_v1.0.zip/file"
+-Texture Pack HD v1.02 "https://www.mediafire.com/file/sc2udr3924zbus9/dbz_bt4_hd_v1.02.zip/file"
 -Explicación de instalación "https://www.mediafire.com/file/iqv5v82qrkvcufy/Explicaci%25C3%25B3n_de_instalaci%25C3%25B3n_de_texturas.txt/file"
 `;
