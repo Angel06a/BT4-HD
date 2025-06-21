30e9d2de576851a82080c78d9b068355e1ce3c5d
Angel06 <ug25228@gmail.com>
2025-06-21 02:44:18 -0500

    Update list.js

--- list.js
+++ list.js
@@ -1,7 +1,8 @@
 // list.js
 const listData = `
 Texture Pack:
--Texture Pack HD v1.04 "https://www.mediafire.com/file/2xobrmodyauj89b/dbz_bt4_hd_v1.04.zip/file"
+-Texture Pack HD v1.05 "https://www.mediafire.com/file/metbjkyo0jrmlaw/dbz_bt4_HD_v1.05.zip/file"
+-Texture Pack Lite v1.05 "https://www.mediafire.com/file/j1xz0ehe2am6235/dbz_bt4_Lite_v1.05.zip/file"
 Extra:
 -Installation explanation @ENG "https://github.com/Angel06a/BT4-HD/blob/main/README.md#%EF%B8%8Finstallation"
 -Explicación de instalación @ESP "https://github.com/Angel06a/BT4-HD/blob/main/README.es.md#%EF%B8%8Finstalaci%C3%B3n"
