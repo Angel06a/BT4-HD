9873d4e64347a66365f0cedabb03b6fbea487ae1
Angel06 <ug25228@gmail.com>
2026-06-19 20:10:39 -0500

    Update texture pack versions in list.js

--- list.js
+++ list.js
@@ -1,8 +1,8 @@
 // list.js
 const listData = `
 Texture Pack:
--Avance Beta14 v1.06 "https://www.mediafire.com/file/lj2qsqh9oflc9b8/%255BAvance_Beta14%255D_v1.06.zip/file"
--Texture Pack Lite v1.05 "https://www.mediafire.com/file/j1xz0ehe2am6235/dbz_bt4_Lite_v1.05.zip/file"
+-Texture Pack HD v1.07 "https://www.mediafire.com/file/ie6dmm5lb6v4ern/dbz_bt4_HD_v1.07.zip/file"
+-Texture Pack Lite v1.07 "https://www.mediafire.com/file/ju06p3b2eqjr23x/dbz_bt4_Lite_v1.07.zip/file"
 Extra:
 -Installation explanation @ENG "https://github.com/Angel06a/BT4-HD/blob/main/README.md#%EF%B8%8Finstallation"
 -Explicación de instalación @ESP "https://github.com/Angel06a/BT4-HD/blob/main/README.es.md#%EF%B8%8Finstalaci%C3%B3n"
